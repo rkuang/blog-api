7521a77934f682e71e5176b051a001db7cae5e98
Ricky Kuang <rkuang26@gmail.com>
2021-01-06 20:20:27 -0800

    add create method for /comments

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,10 +1,12 @@
+const { body, validationResult } = require('express-validator');
 const createError = require('http-errors');
 
 const Comment = require('../models/comment');
+const Post = require('../models/post');
 
 module.exports = {
   get_comment_list: (req, res, next) => {
-    Comment.find({post: req.query.post}).exec((err, comments) => {
+    Comment.find(req.query).exec((err, comments) => {
       if (err) return next(err);
       res.json({comments: comments});
     });
@@ -18,5 +20,29 @@ module.exports = {
         comment: comment
       });
     });
-  }
+  },
+
+  create_comment: [
+    body('content', '`content` cannot be empty').trim().notEmpty().escape(),
+    body('post', '`post` cannot be empty').trim().notEmpty().escape(),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) return res.status(400).json(errors.array());
+      Post.findById(req.body.post, (err, post) => {
+        if (err) return next(err);
+        if (!post) return res.status(404).json({
+          message: `Post ${req.body.post} Not Found`
+        });
+        const comment = new Comment({
+          content: req.body.content,
+          author: req.user._id,
+          post: req.body.post
+        });
+        comment.save((err, comment) => {
+          if (err) return next(err);
+          res.status(201).json(comment);
+        });
+      });
+    }
+  ]
 }
--- routes/comments.js
+++ routes/comments.js
@@ -8,4 +8,6 @@ router.get('/', controller.get_comment_list);
 
 router.get('/:id', controller.get_comment_detail);
 
+router.post('/', controller.create_comment);
+
 module.exports = router;
